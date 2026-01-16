743d40d0c05093068b5dfacdddc65e6c608705f4
CodeYogi <dasbabubag@gmail.com>
2026-01-16 09:27:59 +0000

    basic file upload

---
+++ 01_basic/00_basic.js
@@ -0,0 +1,23 @@
+//✅What is Computer Programming?
+//Computer Programming is a medium for us to communicate with computer. Just like We used "hindi" or "English" language to communicate each other. Similary, computer programming is a way for us to deliver our instruction to the computer.
+
+/*✅What is JavaScript?
+1. JavaScript is a language of web.used to make websites interactive and dynamic. 
+2. It is a powerful Programming Language that transforms static pages into responsive experiences.
+3. javaScript is a high level Programming Language, meaning its syntax is easy for humans to read and write (e.g., if-else, loops).
+4. It build the frontend logic and functionality of websites, controlling how pages react to user actions.
+5. Together with Html (structure) and Css (style), JavaScript acts as the brain that makes web pages come alive.
+6. It can also run outside browser using Node.js, making it useful for server-side programming too.
+*/
+
+/*✅History Of JavaScript?
+Java Script was created in 1995 by Brendan Eich. While working at Netscape, and although it was first called Mocha and then LiveScript, it finally renamed JavaScript mainly for marketing reasons, even though it has no relation to Java. In 1997, ECMA International Standardized the language under the  name ECMAScript so that all browser could follow the same rules, and this standard continues to evolve today. In its early days, JavaScript only ran inside browsers to make web pages interactive, but in 2009 Ryan Dahl introduced Node.js,
+which allowed JavaScript to run on servers as well, making it possible to use one language for both frontend and backend development. Later, Ryan Dahl also Create Deno.js as a more secure and modern alternate to node.js. Brendan Eich went on to co-found Mozila, the company behind Firefox, and today most high-quality documentation for javaScript is avaliable on MDN (Mozilla Development Network). The language itself is maintained and imporved by the TC39 committe, a group of experts form major tech companies who propose and apporve new features, ensuring javaScript continues to grow as one of the most widly used programming language in the world.
+*/
+
+/*✅javaScript version and key feautre.
+1. 1997- ES1 -> first official standardization of javaScript by ECMA international.
+2. 1999- ES3 -> Added regular expressions, better string handling, and try/catch error handling.
+3. 2009- ES5 -> Introduced stric mode, JSON support, and array methods like ForEach, map, filter
+4. 2015- ES6 -> Huge Update: let and const, arrow functions, classes, template literals, modules, promises, default parameters, and more.
+*/

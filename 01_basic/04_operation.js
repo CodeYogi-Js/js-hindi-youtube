e4275065dca093b383418beeab11df6e247d40a0
CodeYogi <dasbabubag@gmail.com>
2026-01-17 16:17:08 +0000

    add file

---
+++ 01_basic/04_operation.js
@@ -0,0 +1,36 @@
+/*✅Operators in javaScript
+In JavaScript, operators are special symbols or keywords used to perform operations on values and variables.
+1.Arithmatic Operator
+2.Assignment Operator
+3.Comparison Operator
+4.Logical Operator
+5.Bitwise Operator
+6.String Operator (+) concatenation
+7.Ternary Operator
+*/
+
+//✅Arithmatic operator
+let value = -3
+let negativeValue = value
+// console.log(negativeValue)
+
+// console.log(3+3)//addition
+// console.log(3-3)//subtraction
+// console.log(3*3)//multiplication
+// console.log(15/3)//division
+// console.log(3**3)//exponentiation
+// console.log(15%3)//modulo operator
+
+//✅Assignment Operator =, +=, -=, /=, *=
+//✅Comparison Operator
+
+
+
+
+
+
+//✅String Operator (Concatenation)
+// const firstName = "Brendan"
+// const lastName = "Eich"
+// const fullName = firstName+" "+ lastName
+// console.log(fullName)
---
+++ 01_basic/05_comparisonOperator.js
@@ -0,0 +1,21 @@
+//✅Comparison Operator
+console.log(2>1)//true
+console.log(2>=1)//true
+console.log(2<1)//false
+console.log(2==1)//false
+console.log(2!=1)//true
+//Problem come not comparison defferent operator
+console.log("2">1);//true
+console.log("02">1);//true
+
+console.log(null>0)
+console.log(null ==0)
+console.log(null >=0)
+
+console.log(undefined == 0)
+console.log(undefined >0)
+console.log(undefined<0)
+
+// strick check
+console.log('2'===2)//false
+console.log('2'!==3)//true

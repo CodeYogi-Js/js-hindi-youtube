2dd9d4d91a6c2fa43feb39247378829e72f700fa
CodeYogi <dasbabubag@gmail.com>
2026-01-17 16:36:29 +0000

    change content

--- 01_basic/05_comparisonOperator.js
+++ 01_basic/05_comparisonOperator.js
@@ -1,21 +1,22 @@
 //✅Comparison Operator
-console.log(2>1)//true
-console.log(2>=1)//true
-console.log(2<1)//false
-console.log(2==1)//false
-console.log(2!=1)//true
+console.log(2>1)//
+console.log(2>=1)//
+console.log(2<1)//
+console.log(2==1)//
+console.log(2!=1)//
 //Problem come not comparison defferent operator
-console.log("2">1);//true
-console.log("02">1);//true
+console.log("2">1);//
+console.log("02">1);//
 
-console.log(null>0)
-console.log(null ==0)
-console.log(null >=0)
+console.log(null>0)//
+console.log(null ==0)//
+// Equality (===) does not convert null to a number—it only matches with undefined.
+console.log(null >=0)//
 
-console.log(undefined == 0)
-console.log(undefined >0)
-console.log(undefined<0)
+console.log(undefined == 0)//
+console.log(undefined >0)//
+console.log(undefined<0)//
 
 // strick check
-console.log('2'===2)//false
-console.log('2'!==3)//true
+console.log('2'===2)//
+console.log('2'!==3)//

9cc775e6aa9a0ac30349e377acf2bb340a52fe7b
CodeYogi <dasbabubag@gmail.com>
2026-01-18 18:50:12 +0000

    add content

--- 01_basic/04_operation.js
+++ 01_basic/04_operation.js
@@ -21,8 +21,33 @@ let negativeValue = value
 // console.log(3**3)//exponentiation
 // console.log(15%3)//modulo operator
 
+
+
 //✅Assignment Operator =, +=, -=, /=, *=
-//✅Comparison Operator
+
+
+
+//✅Comparison Operator comming....just next file
+
+
+
+
+
+//✅logical Operator && -> AND, || ->OR, ! ->NOT
+let isLoggedIn = true;
+let hasAccess = false
+console.log(isLoggedIn && hasAccess);//false
+
+
+
+
+
+
+
+
+//✅Bitwise Operator & -> AND, | -> OR, ^ -> XOR
+
+
 
 
 
@@ -34,3 +59,58 @@ let negativeValue = value
 // const lastName = "Eich"
 // const fullName = firstName+" "+ lastName
 // console.log(fullName)
+
+
+
+
+//✅Ternary Operator
+const myAge = 27
+myAge >= 18 ? console.log("you are adult.") : console.log("You are too much young....")
+
+
+
+/***************extra*********************************/
+//Complex situation
+console.log("1"+2)
+console.log(1 + "2")
+console.log("1" + 2 + 2)
+console.log(1 + 2 + "2")
+console.log(null + 1)
+console.log(undefined + 1)
+
+
+// Type coercion means JavaScript automatically converts one data type to another when needed—especially in operations involving mixed types (like strings and numbers).
+console.log('10' - '4');     // ?
+console.log(false + 'true'); // ?
+console.log(null == 0);      // ?
+console.log(undefined == null); // ?
+
+
+//JavaScript Operator Precedence
+console.log((3+4)*5 %3)
+console.log(+true)
+
+// When operators have same precedence, JavaScript uses associativity:
+// 1. ( )         → Grouping (do first)
+// 2. **          → Exponentiation (right to left)
+// 3. *, /, %     → Multiply, Divide, Modulo (left to right)
+// 4. +, -        → Add, Subtract (left to right)
+
+// - Left-to-right: +, -, *, /
+// - Right-to-left: =, **, ? :
+
+
+
+// console.log(true+) //not possible
+
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+
+//Increment Operator and Decrement Operator //prefix and postfix
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter)
